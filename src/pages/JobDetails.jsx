b7c25ec5801176515d0c4c5c2cf0f03c3d23891e
masum-abrar <masum.abrar02@gmail.com>
2024-05-14 00:22:00 +0600

    Job Details functionality complited

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -6,13 +6,15 @@ export const JobDetails = () => {
     console.log(jobs)
     const {
         _id,
-        Job_Title,
-        description,
-       
-        Application_Deadline,
-        Salary_Range,
-        Name_who_posted_the_job,
-        Job_Posting_Date
+        jobTitle,
+        jobPostingDate,
+       Name,
+        applicationDeadline,
+        salaryRange,
+        jobCategory,
+        jobApplicantsNumber,
+        pictureURL,
+        jobDescription
        
       } = jobs || {}
   return (
@@ -31,25 +33,26 @@ export const JobDetails = () => {
     
 //   </div>
 <div class="max-w-2xl overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 ml-[25%] mt-10">
-    <img class="object-cover w-full h-64" src="https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Article"/>
+    <img class="object-cover w-full h-64" src={pictureURL} alt="Article"/>
 
     <div class="p-6">
         <div>
-            <span class="text-xs font-medium text-white rounded-lg  uppercase  bg-rose-600 p-3 items-center">{Job_Title}</span>
+            <span class="text-xs font-medium text-white rounded-lg  uppercase  bg-rose-600 p-3 items-center">{ jobTitle}</span>
+            <span class="text-xs font-medium text-white rounded-lg  uppercase  bg-rose-600 p-3 items-center ml-[60%]">{ jobCategory}</span>
             
-            <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie parturient et sem ipsum volutpat vel. Natoque sem et aliquam mauris egestas quam volutpat viverra. In pretium nec senectus erat. Et malesuada lobortis.</p>
+            <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">{jobDescription}</p>
         </div>
         <div className='text-xl font-semibold'>
-            <span>{Application_Deadline}</span> <span className='ml-[60%]'>{Job_Posting_Date}</span>
+            <span>{ applicationDeadline}</span> <span className='ml-[60%]'>{ jobPostingDate}</span>
         </div>
 
         <div class="mt-4">
             <div class="flex items-center">
                 <div class="flex items-center">
                     <img class="object-cover h-10 rounded-full" src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=48&q=60" alt="Avatar"/>
-                    <a href="#" class="mx-2 font-semibold text-gray-700 dark:text-gray-200" tabindex="0" role="link">Jone Doe</a>
+                    <a href="#" class="mx-2 font-semibold text-gray-700 dark:text-gray-200" tabindex="0" role="link">{Name}</a>
                 </div>
-                <span class="mx-1 text-xs text-gray-600 dark:text-gray-300">21 SEP 2015</span>
+               
             </div>
            
         </div>
