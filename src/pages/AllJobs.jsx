e0c93a99c82277f1141730bbb1c89be1edd84df4
masum-abrar <masum.abrar02@gmail.com>
2024-05-14 01:14:01 +0600

    Search Functionality added on the all jobs page

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
-import { useLoaderData } from 'react-router-dom'
-import { SingleJob } from './SingleJob'
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { SingleJob } from './SingleJob';
 
 export const AllJobs = () => {
+  const jobs = useLoaderData();
+  const [searchQuery, setSearchQuery] = useState('');
 
-  const jobs = useLoaderData()
+  const filteredJobs = jobs.filter(job =>
+    job.jobTitle.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
-
+  const handleSearchChange = event => {
+    setSearchQuery(event.target.value);
+  };
 
   return (
-    
-  <div className=''>
-      <div className='flex flex-wrap gap-3 justify-center mt-4'>
-      {jobs
-        
-        .map(job => (
-        <SingleJob key={job._id} job={job}></SingleJob>
+    <div className="">
+      <div className="flex justify-center mt-4">
+        <input
+          type="text"
+          placeholder="Search by job title"
+          className="border p-2 rounded-md"
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+      </div>
+      <div className="flex flex-wrap gap-3 justify-center mt-4">
+        {filteredJobs.map(job => (
+          <SingleJob key={job._id} job={job}></SingleJob>
         ))}
       </div>
-  </div>
-  )
-}
+    </div>
+  );
+};
