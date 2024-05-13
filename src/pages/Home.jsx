80c1792559bc9e3255abb31dd39ba5f4b374b58f
masum-abrar <masum.abrar02@gmail.com>
2024-05-13 12:14:21 +0600

    Dark/Light theme added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import { Navbar } from '../components/Navbar'
 import { Banner } from '../components/Banner'
 import { TabCategories } from '../components/TabCategories'
@@ -9,6 +9,27 @@ import { Test } from './Test'
 export const Home = () => {
   const jobs = useLoaderData()
   console.log(jobs)
+
+  const [theme, setTheme] = useState(() => {
+    
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme || 'light';
+});
+
+useEffect(() => {
+    
+    document.querySelector('html').setAttribute('data-theme', theme);
+}, [theme]);
+
+const handleToggle = (e) => {
+    const newTheme = e.target.checked ? 'night' : 'light';
+    setTheme(newTheme);
+    
+    localStorage.setItem('theme', newTheme);
+};
+console.log(theme)
+
+
   return (
     <div>
 
