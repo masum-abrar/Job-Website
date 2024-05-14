195b331f1f467ac8f41c10b817e7c61bd781b64e
masum-abrar <masum.abrar02@gmail.com>
2024-05-14 22:12:09 +0600

    Tanstack query added

--- src/main.jsx
+++ src/main.jsx
@@ -34,7 +34,7 @@ const router = createBrowserRouter([
       {
         path : "/",
         element : <Home></Home>,
-        loader : () => fetch(`http://localhost:5000/jobs`),
+      //  loader : () => fetch(`http://localhost:5000/jobs`),
    
       },
       {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,48 +9,49 @@ import { NewSection } from '../components/NewSection'
 import { HowitWork } from '../components/HowitWork'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { JobCard } from '../components/JobCard'
-//import { useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 export const Home = () => {
-  const jobs = useLoaderData()
-  console.log(jobs)
-
-  // const { isPending, isError, error, data: foods } = useQuery({
-  //       queryKey: ['foods'],
-  //       queryFn: async () => {
-  //           const res = await fetch('http://localhost:8000/foods');
-  //           return res.json();
-  //       }
-  //   })
-  //   console.log(foods);
-
-
-  //   if (isPending) {
-  //       return <span className="loading loading-spinner text-primary"></span>
-  //   }
-
-  //   if (isError) {
-  //       return <p>{error.message}</p>
-  //   }
-
-  const [theme, setTheme] = useState(() => {
+ // const jobs = useLoaderData()
+ // console.log(jobs)
+ const [theme, setTheme] = useState(() => {
     
-    const savedTheme = localStorage.getItem('theme');
-    return savedTheme || 'light';
+  const savedTheme = localStorage.getItem('theme');
+  return savedTheme || 'light';
 });
 
 useEffect(() => {
-    
-    document.querySelector('html').setAttribute('data-theme', theme);
+  
+  document.querySelector('html').setAttribute('data-theme', theme);
 }, [theme]);
 
 const handleToggle = (e) => {
-    const newTheme = e.target.checked ? 'night' : 'light';
-    setTheme(newTheme);
-    
-    localStorage.setItem('theme', newTheme);
+  const newTheme = e.target.checked ? 'night' : 'light';
+  setTheme(newTheme);
+  
+  localStorage.setItem('theme', newTheme);
 };
 console.log(theme)
+
+
+  const { isPending, isError, error, data: jobs } = useQuery({
+        queryKey: ['jobs'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/jobs');
+           return res.json();
+        }
+    })
+
+
+   if (isPending) {
+       return <span className="loading loading-spinner text-primary"></span>
+  }
+
+  if (isError) {
+       return <p>{error.message}</p>
+   }
+
+  
 
 
   return (
