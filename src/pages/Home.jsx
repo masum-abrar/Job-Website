5cdee6c8d4872ca7822310335de4e82e64137589
masum-abrar <masum.abrar02@gmail.com>
2024-05-14 21:33:40 +0600

    tab added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,8 @@ import { AllJobs } from './AllJobs'
 import { Test } from './Test'
 import { NewSection } from '../components/NewSection'
 import { HowitWork } from '../components/HowitWork'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { JobCard } from '../components/JobCard'
 //import { useQuery } from "@tanstack/react-query";
 
 export const Home = () => {
@@ -55,8 +57,72 @@ console.log(theme)
     <div>
 
 <Banner></Banner>
-<TabCategories jobs={jobs}></TabCategories>
+{/* <TabCategories jobs={jobs}></TabCategories> */}
 {/* <Test></Test> */}
+<div>
+<div>
+            <Tabs>
+                <div className='container px-6 py-10 mx-auto'>
+                    <h1 className='text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl '>
+                        Browse Jobs By Categories
+                    </h1>
+                    <p className='max-w-2xl mx-auto my-6 text-center text-gray-500 '>
+                        Three categories available for the time being. They are Web Development, Graphics Design and Digital Marketing. Browse them by clicking on the tabs below.
+                    </p>
+                    <div className='flex items-center justify-center'>
+                        <TabList>
+                            <Tab>On-Site Job</Tab>
+                            <Tab>Remote Job</Tab>
+                            <Tab>Hybrid</Tab>
+                            <Tab>Part-Time</Tab>
+                        </TabList>
+                    </div>
+
+                
+         
+              <TabPanel>
+                 <div className='flex gap-3 justify-center'>
+                 {jobs
+              .filter(j => j.jobCategory === "On Site")
+              .map(job => (
+                <JobCard key={job._id} job={job} />
+              ))}
+                 </div>
+                    </TabPanel>
+           
+                 
+                    <TabPanel>
+               <div  className='flex gap-3 justify-center'>
+               {jobs
+              .filter(j => j.jobCategory === "Remote")
+              .map(job => (
+                <JobCard key={job._id} job={job} />
+              ))}
+               </div>
+                    </TabPanel>
+                    <TabPanel>
+                <div className='flex gap-3 justify-center'>
+                {jobs
+              .filter(j => j.jobCategory=== "Hybrid")
+              .map(job => (
+                <JobCard key={job._id} job={job} />
+              ))}
+                </div>
+                    </TabPanel>
+                    <TabPanel>
+               <div className='flex gap-3 justify-center'>
+               {jobs
+              .filter(j =>j.jobCategory === "Part-Time")
+              .map(job => (
+                <JobCard key={job._id} job={job} />
+              ))}
+               </div>
+                    </TabPanel>
+                 </div>
+               
+            </Tabs>
+        </div>
+</div>
 <NewSection></NewSection>
 <HowitWork></HowitWork>
     </div>
